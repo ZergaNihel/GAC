c4e1145369e76452cbd0cbb862064cf02c7c2f8e
ZergaNihel <nihel.zerga98@gmail.com>
2019-03-19 21:31:02 +0100

    ajout des modeles

---
+++ app/Groupe_etu.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Groupe_etu extends Model
+{
+    public $table = "groupe_etus";
+}
---
+++ app/Td_tp.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Td_tp extends Model
+{
+     public $table = "td_tps";
+}
